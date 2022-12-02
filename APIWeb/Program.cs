01f1d056194922431a0729efd52cdfb552cffe64
Fernando <fernando.fsb24@gmail.com>
2022-12-01 21:21:48 -0300

    actualizacion

--- APIWeb/Program.cs
+++ APIWeb/Program.cs
@@ -9,7 +9,9 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddCors(o => o.AddPolicy("MyPolicy", builder =>
 {
-    builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+    builder.AllowAnyOrigin()
+    .AllowAnyMethod()
+    .AllowAnyHeader();
 }));
 
 var app = builder.Build();
@@ -21,17 +23,12 @@ if (app.Environment.IsDevelopment())
 {
     
 }
-//if (app.Environment.IsProduction())
-//{
-//    app.UseExceptionHandler("/Error");
-//    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-//    app.UseHsts();
-//}
+
 app.UseSwagger();
 
 app.UseSwaggerUI();
 
-app.UseCors("MyPolice");
+app.UseCors("MyPolicy");
 
 app.UseHttpsRedirection();
 
